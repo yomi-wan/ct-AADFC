2d05da797c410e57e33196d4b87a1bb6dc8b18b4
Yomi Jordan <yomi.jordan@gmail.com>
2020-11-17 16:16:16 -0700

    events page

--- template-parts/content-events.php
+++ template-parts/content-events.php
@@ -10,27 +10,27 @@
  */
 ?>
 <main <?php post_class();?> id="post-<?php the_ID();?>">
-<div class="container">
 
-<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 
-    <div class="events-content text-white row">
-       <div class="col-12 col-md-8">
-            <?php the_content(); ?>
-            <!-- display events page content -->
-       </div>
-       
-  
-     <div class="col-md-3 alert-bubble"><?php the_field('covid_alert');?></div>
-   
+    <div class="container-fluid container-md">
+        <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
     
-       
+        <div class="events-content text-white row">
+            <div class="col-12 col-md-8">
+                <!-- display events page content -->
+                <?php the_content(); ?>
+            </div>
+            <div class="col-md-3">
+                <div class="alert-bubble"><?php the_field('covid_alert');?></div>
+            </div>
+        </div>
     </div>
 
-    <div class="bg-white">
+    <div class="bg-white container-fluid container-md">
+        <!-- Modern Events Calendar -->
         <?php echo do_shortcode('[MEC id="155"]'); ?>
     </div>
 
-</div>
+
 
 </main>
