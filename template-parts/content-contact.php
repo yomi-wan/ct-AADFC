e543459337962b79c7895311158c8b655d3de9b8
Yomi Jordan <yomi.jordan@gmail.com>
2020-12-07 15:27:46 -0700

    converted contact css to sass

--- template-parts/content-contact.php
+++ template-parts/content-contact.php
@@ -1,29 +1,33 @@
 <?php
 /****
  * 
- * Template part for displaying content in the Contact Page
+ * Template part for displaying content in the contect-page
  * 
- * @link https://developer.wordpress.org/theme/basics/template-hierarchy/
  * 
  * @package AADFC
  * 
  */
 ?>
+
 <main <?php post_class();?> id="post-<?php the_ID();?>">
-<div class="container">
-<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-    <div class="row row-md-reverse">
-        <div class="col-md-6">
-            <div class="contact-info">
-                <h5 class="contact-info-content"><?php the_field('contact_number'); ?></h5>
-                <div class="border-line"></div>
-                <h5 class="contact-info-content"> <?php the_field('contact_location'); ?></h5>
+    <div class="container-fluid container-lg">
+        <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+        <div class="d-md-flex justify-content-md-between">
+            <div class="col-md-6 col-lg-5 order-md-2">
+                <div class="contact-info d-flex flex-column justify-content-center">
+                    <h4 class="contact-info-heading d-none d-md-block"><?php the_field('contact_info_heading'); ?></h4>
+                    <p class="contact-info-content"><i class="material-icons">call</i> <?php the_field('contact_number'); ?></p>
+                    <div class="border-line d-md-none"></div>
+                    <p class="contact-info-content"><i class="material-icons">location_on</i> <?php the_field('contact_location'); ?></p>
+                </div>
+            </div>
+
+            <div class="contact-form col-md-6 order-md-1">
+                <?php the_content(); ?>
             </div>
         </div>
-        <div class="col-md-6">
-            <?php the_content(); ?>
-        </div>
     </div>
-</div>
 </main>
+
+
 
