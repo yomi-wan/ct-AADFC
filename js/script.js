40d674d65a700cce655364fe6b3d7d694db3e612
Yomi Jordan <yomi.jordan@gmail.com>
2020-12-12 10:22:19 -0700

    icons added to mobile menu

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ jQuery(document).ready(function(){
     // displays dot style next to titles H2, H3
     jQuery("h2").append("<div class=\"ball\">");
     jQuery("h3").append("<div class=\"ball\">");
-
+    
+    // displays icons in mobile menu
+    jQuery("#menu-item-12 .nav-link").prepend('<i class="material-icons d-lg-none">home</i>'); //home
+    jQuery("#menu-item-45 .nav-link").prepend('<i class="material-icons d-lg-none">people</i>'); //about
+    jQuery("#menu-item-234 .nav-link").prepend('<i class="material-icons d-lg-none">public</i>'); //african resources
+    jQuery("#menu-item-42 .nav-link").prepend('<i class="material-icons d-lg-none">event</i>'); //events
+    jQuery("#menu-item-292 .nav-link").prepend('<i class="material-icons d-lg-none">photo_library</i>'); //media
+    jQuery("#menu-item-23 .nav-link").prepend('<i class="material-icons d-lg-none">mail</i>'); //contact
 
     // removes col-sm for Modern Events Calendar Plugin
     jQuery('#mec_skin_events_147 .col-md-4').removeClass('col-sm-4'); 
